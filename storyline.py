eaeed90357a552cc9ac4c05c93d81398c21fc059
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-04-17 12:16:17 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -3,4 +3,13 @@ import time
 print('Your adventure begins now {username}.')
 time.sleep(1)
 print('You encounter a skeleton, a fierce battle begins.')
-input(print('Pick a move (Fight, Defend, Run)'))
+time.sleep(3)
+x = input(print('Pick (Fight, Defend, Run): '))
+if x == 'Fight':
+    print('')
+elif x == 'Defend':
+    print('You lost 2 health points')
+elif x == 'Run':
+    print("WRONG CHOICE, you're not allowed to run")
+    print('You lost 5 health points')
+    print()
