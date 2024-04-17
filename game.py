720e3bc7f55729aa7290ea2707d65daf944e2378
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-04-17 12:00:39 -0400

    a

--- game.py
+++ game.py
@@ -5,16 +5,21 @@ from classes import User
 print("Hello player, welcome!")
 time.sleep(1)
 username = input("What will your username be? ")
+time.sleep(2)
 print(f"Hello {username}, welcome to Zhou Dynasty!")
 Guide = input("Would you like a tutorial? Y/N: ")
 if Guide.upper() == "Y":
     print("")
 
+
 hp = 100
 ArcherDamage = 20
-MageDamage = 
-SwordsmanDamage = a
+MageDamage = 15
+SwordsmanDamage = 30
 Class = input("Choose class (Archer, Mage, Swordsman): ")
 if Class == "Archer": 
-    s = User(username, Class,  )
+    s = User(username, Class, )
 
+print('Your adventure begins now {username}.')
+time.sleep(1)
+print('You encounter a skeleton, ')
