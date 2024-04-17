ba9e8360a9c747ef62a6b7472a6473c7ba945a9b
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-04-17 12:17:08 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,6 @@
 import time
 import random
-
-class User:
-    def __init__(self, username, health, damage, Class, weapon):
-        self.username = username
-        self.health = health
-        self.damage = damage
-        self.Class = Class
-        self.weapon = weapon
-    def __str__(self):
-        return f"{self.username}, {self. health}, {self.damage}, {self.Class}, {self.weapon}"
-
+from classes import User
 
 print("Hello player, welcome!")
 time.sleep(1)
@@ -21,7 +11,6 @@ Guide = input("Would you like a tutorial? Y/N: ")
 if Guide.upper() == "Y":
     print("")
 
-<<<<<<< HEAD
 def classselction():
     Class = input("Choose class (Archer, Mage, Swordsman): ")
     if Class == "Archer": 
@@ -36,9 +25,6 @@ def classconfirmation():
     if confirm.upper() != "Y":
         classselction()
 
-
-=======
-
 hp = 100
 ArcherDamage = 20
 MageDamage = 15
@@ -46,8 +32,4 @@ SwordsmanDamage = 30
 Class = input("Choose class (Archer, Mage, Swordsman): ")
 if Class == "Archer": 
     s = User(username, Class, )
->>>>>>> 5f55469f8b4c66c00d65d21a9b77d5c96f203678
 
-print('Your adventure begins now {username}.')
-time.sleep(1)
-print('You encounter a skeleton, a fierce battle begins.')
