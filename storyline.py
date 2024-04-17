86d684025b02b197be57455fa04d8f793f52be37
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-04-17 12:05:20 -0400

    Create storyline.py

---
+++ storyline.py
@@ -0,0 +1,6 @@
+import time
+
+print('Your adventure begins now {username}.')
+time.sleep(1)
+print('You encounter a skeleton, a fierce battle begins.')
+input(print('Pick a move (Fight, Defend, Run)'))
