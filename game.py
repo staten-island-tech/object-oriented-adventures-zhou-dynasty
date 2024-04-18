32c522398d3522ee5674146c2e0d755f51af876a
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-04-18 12:28:37 -0400

    a

--- game.py
+++ game.py
@@ -1,35 +1,54 @@
 import time
 import random
-from classes import User
+
+class User:
+    def __init__(self, username, health, damage, Class, weapon):
+        self.username = username
+        self.health = health
+        self.damage = damage
+        self.Class = Class
+        self.weapon = weapon
+    def __str__(self):
+        return f"{self.username}, {self. health}, {self.damage}, {self.Class}, {self.weapon}"
+    
+
+
+class Class:
+    def __init__(self):
+        self.Class = True
+
+    def classselction():
+        Class = input("Choose class (Archer, Mage, Swordsman): ")
+        if Class == "Archer": 
+            s = User(username, 20, 20, Class, "Short Bow")
+        elif Class == "Mage":
+            s = User(username, 15, 30, Class, "Magic Wand")
+        elif Class == "Swordsman":
+            s = User(username, 30, 10, Class, "Wooden Sword")
+    
+    def classconfirmation():
+        p = Class
+        while True:
+            confirmation = input("Are you sure you want to select this class? Y/N: ")
+            if confirmation.upper() != "Y":
+                p.classselction()
+            else: 
+                break
+def identifying_class():
+    d = Class
+    d.classselction()
+    d.classconfirmation() 
+    
 
 print("Hello player, welcome!")
 time.sleep(1)
 username = input("What will your username be? ")
-time.sleep(2)
 print(f"Hello {username}, welcome to Zhou Dynasty!")
-Guide = input("Would you like a tutorial? Y/N: ")
-if Guide.upper() == "Y":
-    print("")
+time.sleep(1)
+identifying_class()
 
-def classselction():
-    Class = input("Choose class (Archer, Mage, Swordsman): ")
-    if Class == "Archer": 
-        s = User(username, 20, 20, Class, "Short Bow")
-    elif Class == "Mage":
-        s = User(username, 15, 30, Class, "Magic Wand")
-    elif Class == "Swordsman":
-        s = User(username, 30, 10, Class, "Wooden Sword")
 
-def classconfirmation():
-    confirm = input("Are you sure you want to pick this class? Y/N: ")
-    if confirm.upper() != "Y":
-        classselction()
 
-hp = 100
-ArcherDamage = 20
-MageDamage = 15
-SwordsmanDamage = 30
-Class = input("Choose class (Archer, Mage, Swordsman): ")
-if Class == "Archer": 
-    s = User(username, Class, )
+
+
 
