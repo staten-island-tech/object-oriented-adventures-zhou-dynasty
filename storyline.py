1832649a0eba6b4809f1635cc3e0cdd956dd3bc8
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-04-19 12:29:12 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -6,28 +6,33 @@ class storyline():
 
     def archer():
         print('Your adventure begins now {username}.')
-    time.sleep(2)
-    print('You encounter a skeleton, a fierce battle begins.')
-    time.sleep(3)
-    x = input('Choose your move (Shoot, Defend, Run): ')
-    if x == 'Shoot':
-        time.sleep(1)
+        print('You encounter a skeleton, a fierce battle begins.')
+        time.sleep(3)
+        x = input('Choose your move (Shoot, Defend, Run): ').upper()
+        if x == 'Shoot':
+            time.sleep(1)
         print('You have done 20 damage.')
         time.sleep(2)
         print('Skeleton attacks, dealing 2 damage.')
         time.sleep(2)
-        print('Player: 8/10 hp , Skeleton: 10/30 hp.')
-        y = input('Pick your next move (Shoot, Defend, Run): ')
-    elif x == 'Defend':
+        print('Player: 8/10 hp , Skeleton: 10/30 hp')
+        y = input('Pick your next move (Shoot, Defend, Run): ').upper()
+        elif x == 'Defend':
         time.sleep(2)
         print('You have decided to defend, no damage dealt.')
         time.sleep(2)
         print('Skeleton attacks, dealing 1 damage.')
         time.sleep(2)
-        print('Player: 9/10 hp, Skeleton: 30 hp.')
-        y = input('Pick your next move (Shoot, Defend, Run): ')
-        if y == ''
-    elif x == 'Run':
+        print('Player: 9/10 hp, Skeleton: 30 hp')
+        y = input('Pick your next move (Shoot, Defend, Run): ').upper()
+        if y == 'Shoot':
+            time.sleep(2)
+        print('You have done 20 damage.')
+        time.sleep(2)
+        print('Skeleton attacks, dealing 2 damage')
+        time.sleep(2)
+        print('Player: 7/10 hp, Skeleton: 10/30 hp')
+        elif x == 'Run':
         print('WRONG CHOICE, YOU ARE NOT ALLOWED TO RUN!!!!')
         time.sleep(3)
         print('You have taken 3 damage.')
@@ -36,4 +41,4 @@ class storyline():
         time.sleep(2)
         print('Player: 5/10, Skeleton: 30 hp')
         time.sleep(1)
-        y = input('Pick your next move (Shoot, Defend, Run): ')
+        y = input('Pick your next move (Shoot, Defend, Run): ').upper()
