b840214e96f2b7022408e01b0c2ddaa7dcf03cce
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-05-03 12:05:00 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -15,6 +15,7 @@ class storyline():
         print('You encounter a skeleton, a fierce battle begins.')
         time.sleep(3)
     def shoot():
+            "shoot"
             time.sleep(1)
             print('You have done 20 damage.')
             time.sleep(2)
@@ -22,6 +23,7 @@ class storyline():
             time.sleep(2)
             print('Player: 18/20 hp , Skeleton: 10/30 hp')
     def shoot2():
+            "shoot, shoot"
             time.sleep(1)
             print('You have done 20 damage.')
             time.sleep(2)
@@ -29,6 +31,7 @@ class storyline():
             time.sleep(2)
             print('Player: 18/20 hp , Skeleton: 0/30 hp')
     def shoot3():
+            "shoot, defend"
             time.sleep(1)
             print('You have done 0 damage.')
             time.sleep(2)
@@ -36,6 +39,7 @@ class storyline():
             time.sleep(2)
             print('Player: 17/20 hp , Skeleton: 10/30 hp')     
     def shoot4():
+            "shoot,run"
             time.sleep(1)
             print('No Running')
             time.sleep(2)
@@ -43,6 +47,7 @@ class storyline():
             time.sleep(2)
             print('Player: 14/20 hp , Skeleton: 10/30 hp')            
     def defend():
+            "defend"
             time.sleep(2)
             print('You have decided to defend, no damage dealt.')
             time.sleep(2)
@@ -50,6 +55,7 @@ class storyline():
             time.sleep(2)
             print('Player: 19/20 hp, Skeleton: 30 hp')
     def defend2():
+            "defend, shoot"
             time.sleep(2)
             print('You have chosen to shoot.')
             time.sleep(2)
@@ -59,6 +65,7 @@ class storyline():
             time.sleep(2)
             print('Player: 17/20 hp, Skeleton: 10 hp')
     def defend3():
+            "defend, defend"
             time.sleep(2)
             print('You have chosen to Defend again for some reason.')
             time.sleep(2)
@@ -68,6 +75,7 @@ class storyline():
             time.sleep(2)
             print('Player: 18/20 hp, Skeleton: 10 hp')
     def defend4():
+            "defend,run"
             time.sleep(2)
             print('You have chosen to Run.')
             time.sleep(2)
@@ -77,6 +85,7 @@ class storyline():
             time.sleep(2)
             print("I'm taking away your privilege to run.")
     def run():
+            "run"
             print('WRONG CHOICE, YOU ARE NOT ALLOWED TO RUN!!!!')
             time.sleep(3)
             print('You have taken 3 damage.')
@@ -86,6 +95,7 @@ class storyline():
             print('Player: 15/20, Skeleton: 30 hp')
             time.sleep(1)
     def run2():
+            "run, shoot"
             time.sleep(1)
             print('You have done 20 damage.')
             time.sleep(2)
@@ -93,8 +103,9 @@ class storyline():
             time.sleep(2)
             print('Player: 18/20 hp , Skeleton: 10/30 hp')
     def run3():
+            "run, defend"
             time.sleep(2)
-            print('You have chosen to Defend again for some reason.')
+            print('You have chosen to Defend for some reason.')
             time.sleep(2)
             print('You have dealt 0 damage.')
             time.sleep(2)
@@ -102,6 +113,7 @@ class storyline():
             time.sleep(2)
             print('Player: 18/20 hp, Skeleton: 10/30 hp')
     def run4():
+            "run, run"
             time.sleep(2)
             print('You have chosen to Run.')
             time.sleep(2)
@@ -109,12 +121,17 @@ class storyline():
             time.sleep(2)
             print('Why coward out of a fight?')
             time.sleep(2)
-            print("I'm taking away your privilege to run.")
-            time.sleep(2)
             print('You have taken 5 damage')
             time.sleep(3)
             print('Skeleton decides to take pity due to your stupidity')
             time.sleep(2)
             print('Player 10/20, Skeleton: 10/30 hp')
-    def run5():
-           
+    def runcontinuance():
+            "input next move"
+            "run, run, run"
+            print('You have chosen to run for a third time?')
+            time.sleep(2)
+            print('This is the end of your miserable life.')
+            time.sleep(2)
+            print('If you wish to restart your run and be smarter this time.')
+            input('')
