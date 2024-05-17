5181edee6b115a3c425a69abbe2ea18e27c207b1
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-05-17 12:08:41 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,5 +1,12 @@
 #Mobs
-class Skeleton():
+class Mob():
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    def __str__(self):
+        return f"{self.name}, {self.health}"
+
+class Skeleton(Mob):
     def __init__(self, name, health, damage, weapon):
         self.name = name
         self.health = health
